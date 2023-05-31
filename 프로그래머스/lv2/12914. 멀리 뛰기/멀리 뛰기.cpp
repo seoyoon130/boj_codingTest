04a224a47c44a44548081312dac33cb73ab6d780
Seoyoon Heo <76056286+seoyoon130@users.noreply.github.com>
2023-05-31 10:55:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[2001];
+
+
+int solution(int n) {
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+
+    return dp[n];
+}
