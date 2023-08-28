53789305b1e02aa8c31b654294d655f6609f175f
Seoyoon Heo <76056286+seoyoon130@users.noreply.github.com>
2023-08-28 16:25:47 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+struct process{
+    int prio;
+    int idx;
+};
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<process>q;
+    priority_queue<int>pq;
+    for(int i = 0;i<priorities.size();i++){
+        q.push({priorities[i], i});
+        pq.push(priorities[i]);
+    }
+    while(!q.empty()){
+        if(q.front().prio == pq.top()){
+            answer++;
+            if(q.front().idx == location) break;
+            q.pop();
+            pq.pop();
+        }
+        else {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    return answer;
+}
