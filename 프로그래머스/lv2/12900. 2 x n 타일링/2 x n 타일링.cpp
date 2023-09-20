c82def0b01905fd829148ce3632d96ba4c5af076
Seoyoon Heo <76056286+seoyoon130@users.noreply.github.com>
2023-09-20 12:09:41 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.27 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[600000] = {0};
+
+int solution(int n) {
+    int answer = 0;
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i = 2;i<=n;i++){
+        dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        
+    }
+    answer = dp[n];
+    return answer;
+}
